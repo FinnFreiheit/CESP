57aa08d3d4857d6dba9ae4b10096d1cea1897369
FinnFreiheit <finn.owe.freiheit@gmail.com>
2020-09-23 10:27:48 +0200

    Umsetzung der If Bedingungen

--- Fib01.asm
+++ Fib01.asm
@@ -7,9 +7,12 @@
 #        Hint: Use an assembler directive and a label (use the name fibonacci for the label).
 .align 2
 # ADD YOUR STEP 1 CODE HERE
+.globl fibonacci
+
+fibonacci: 
+	.space 80		#dem Lable fibonacci 20 * 4 Byte speicher bereit gestellt
 
 .text
-
 
 main:
 	#STEP 2: Implement the call of the fib function
@@ -18,7 +21,10 @@ main:
 	#              2) Use 
 	
 	# ADD YOUR STEP 2 CODE HERE
-
+	la a1,fibonacci		#die Adresse von fibonacci[0] in a1 speichern
+	li a2, 2		#a1 = n = 20, Argument bei Funktionsaufruf
+	jal ra,fib		#jump and link, gehe zum Unterprogramm fib und speicher Ruecksprungadresse in ra.
+	
 	
 	#STEP 6: Print the results
 	
@@ -37,11 +43,25 @@ fib:
 	#STEP 3: First initialize fibonacci[0] and fibonacci[1] with 1 dependent on argument n
 
 	# ADD YOUR STEP 3 CODE HERE
+	li t1,1			#den Wert 1 in t1 speichern
+	
+	ble a2,zero,exit	#Wenn n > 0 --> result[0] = 1 // Wnen n <= 0 --> exit
+	sw t1,0(a1)		#den Wert 1 in fibonacci[0] speichern
+	ble a2, t1,exit		#Wenn n > 1 --> result[1] = 1 // Wnen n <= 1 --> exit
+	sw t1,4(a1)		#den Wert 1 in fibonacci[0] speichern
+	
+	exit: 
+		add zero, zero, zero
+	
+
+		
+	
 	
 _fib_for_loop:
 	#STEP 4: implement a for loop calculate fibonacci[2] ... fibonacci[n-1] if applicable
 
 	# ADD YOUR STEP 4 CODE HERE
+	
 	
 _fib_end:
 	#STEP 5: Jump back into main using the address you have saved before
